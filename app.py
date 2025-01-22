47ca5b09e46cef21111f8844526ffea300cabb86
GALIH RIDHO UTOMO <92840324+4211421036@users.noreply.github.com>
2025-01-23 00:54:07 +0700

    Update app.py

--- app.py
+++ app.py
@@ -4,16 +4,22 @@ import cv2
 app = Flask(__name__)
 
 def gen_frames():
-    camera = cv2.VideoCapture(0)
-    while True:
-        success, frame = camera.read()
-        if not success:
-            break
-        else:
-            ret, buffer = cv2.imencode('.jpg', frame)
-            frame = buffer.tobytes()
-            yield (b'--frame\r\n'
-                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+    camera = cv2.VideoCapture(0)  # Gunakan kamera default
+    if not camera.isOpened():
+        raise RuntimeError("Camera could not be opened.")
+    try:
+        while True:
+            success, frame = camera.read()
+            if not success:
+                break
+            else:
+                # Proses frame di sini (misalnya, deteksi landmark wajah)
+                ret, buffer = cv2.imencode('.jpg', frame)
+                frame = buffer.tobytes()
+                yield (b'--frame\r\n'
+                       b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+    finally:
+        camera.release()
 
 @app.route('/')
 def index():
@@ -24,4 +30,4 @@ def video_feed():
     return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000, debug=True)
