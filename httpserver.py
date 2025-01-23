3b4f6d3a3021b30a52535072ff336845a0d4591f
GALIH RIDHO UTOMO <92840324+4211421036@users.noreply.github.com>
2025-01-23 08:20:13 +0700

    Update httpserver.py

--- httpserver.py
+++ httpserver.py
@@ -1,31 +1,18 @@
+from http.server import SimpleHTTPRequestHandler, HTTPServer
 import threading
-import http.server
-import socketserver
+
 
 class HttpServer:
-
-    def singleStaticPage(self):
-        self.handler = http.server.SimpleHTTPRequestHandler
-        with socketserver.TCPServer(("", self.port), self.handler) as httpd:
-            self.httpd = httpd
-            print("Starting http server on port", self.port)
-            self.httpd.serve_forever()
-
     def __init__(self, port):
-        self.httpserver = None
-        self.port = port
-
-    def __del__(self):
-        self.httpd.shutdown()
+        self.server = HTTPServer(('', port), SimpleHTTPRequestHandler)
+        self.thread = threading.Thread(target=self.server.serve_forever)
+        self.thread.daemon = True
 
     def start(self):
-        if self.httpserver is None:
-            self.httpserver = threading.Thread(target=self.singleStaticPage)
-            self.httpserver.start()
+        self.thread.start()
+        print(f"HTTP server started on port {self.server.server_port}")
 
     def stop(self):
-        if(self.httpd != None):
-            self.httpd.shutdown()
-            print("httpd shut down")
-        self.httpserver.join()
-        self.httpserver = None
+        self.server.shutdown()
+        self.thread.join()
+        print("HTTP server stopped")
